51e5148ad0f9eda77961292999a343be4fb764a1
AdrianGalus <adriangalus8@gmail.com>
2018-11-13 23:00:40 +0100

    add CustomerDetails class

---
+++ src/main/java/traveller/model/CustomerDetails.java
@@ -0,0 +1,45 @@
+package traveller.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDetails {
+
+    private Integer id;
+    private String phone;
+    private String email;
+    private List<Tour> tours = new ArrayList<>();
+
+    public Integer getId() {
+
+        return id;
+    }
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+    public String getPhone() {
+
+        return phone;
+    }
+    public void setPhone(String phone) {
+
+        this.phone = phone;
+    }
+    public String getEmail() {
+
+        return email;
+    }
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+    public List<Tour> getTours() {
+
+        return tours;
+    }
+    public void setTours(List<Tour> tours) {
+
+        this.tours = tours;
+    }
+}
