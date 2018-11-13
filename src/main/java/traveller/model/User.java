68995c9d0ffc96fd4a3a9b31b81d015add107f63
AdrianGalus <adriangalus8@gmail.com>
2018-11-13 22:58:29 +0100

    add User class

---
+++ src/main/java/traveller/model/User.java
@@ -0,0 +1,33 @@
+package traveller.model;
+
+public class User {
+
+    private Integer id;
+    private String login;
+    private String email;
+
+    public Integer getId() {
+
+        return id;
+    }
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+    public String getLogin() {
+
+        return login;
+    }
+    public void setLogin(String login) {
+
+        this.login = login;
+    }
+    public String getEmail() {
+
+        return email;
+    }
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+}
