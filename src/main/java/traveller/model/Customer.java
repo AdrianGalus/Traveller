26c715b6a22b3c93a46500ab7cf44c4889cd7d1e
AdrianGalus <adriangalus8@gmail.com>
2018-11-13 23:00:22 +0100

    add Customer class

---
+++ src/main/java/traveller/model/Customer.java
@@ -0,0 +1,33 @@
+package traveller.model;
+
+public class Customer {
+
+    private Integer id;
+    private String fistName;
+    private String lastName;
+
+    public Integer getId() {
+
+        return id;
+    }
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+    public String getFistName() {
+
+        return fistName;
+    }
+    public void setFistName(String fistName) {
+
+        this.fistName = fistName;
+    }
+    public String getLastName() {
+
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+}
