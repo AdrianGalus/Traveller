2d07ca236d9076b3c35b3c334151fc6768ab26dc
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 10:39:05 +0100

    add getters and setters to CustomerDTO

--- src/main/java/traveller/dtos/CustomerDTO.java
+++ src/main/java/traveller/dtos/CustomerDTO.java
@@ -19,4 +19,53 @@ public class CustomerDTO {
     @NotNull @NotBlank @Email
     private String email;
     private List<Tour> tours;
+
+    public Integer getId() {
+
+        return id;
+    }
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+    public String getFirstName() {
+
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+    public String getPhone() {
+
+        return phone;
+    }
+    public void setPhone(String phone) {
+
+        this.phone = phone;
+    }
+    public String getEmail() {
+
+        return email;
+    }
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+    public List<Tour> getTours() {
+
+        return tours;
+    }
+    public void setTours(List<Tour> tours) {
+
+        this.tours = tours;
+    }
 }
