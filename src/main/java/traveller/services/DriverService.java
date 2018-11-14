390b1d7287fac9df9bd039194f27ae60ace52355
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 23:12:20 +0100

    add DriverService

---
+++ src/main/java/traveller/services/DriverService.java
@@ -0,0 +1,31 @@
+package traveller.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import traveller.dtos.DriverDTO;
+import traveller.model.Driver;
+import traveller.model.DriverDetails;
+import traveller.repositories.DriverDetailsRepository;
+import traveller.repositories.DriverRepository;
+
+@Service
+public class DriverService {
+
+    @Autowired
+    DriverRepository driverRepository;
+    @Autowired
+    DriverDetailsRepository driverDetailsRepository;
+
+    public void addDriver(DriverDTO form) {
+
+        Driver driver = new Driver();
+        driver.setFirstName(form.getFirstName());
+        driver.setLastName(form.getLastName());
+        driverRepository.save(driver);
+        DriverDetails driverDetails = new DriverDetails();
+        driverDetails.setDriver(driver);
+        driverDetails.setPhone(form.getPhone());
+        driverDetails.setEmail(form.getEmail());
+        driverDetailsRepository.save(driverDetails);
+    }
+}
