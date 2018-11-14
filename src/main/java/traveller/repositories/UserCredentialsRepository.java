b14124e7218223a103d4f7011bac3b4e33395316
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 18:52:56 +0100

    correct query

--- src/main/java/traveller/repositories/UserCredentialsRepository.java
+++ src/main/java/traveller/repositories/UserCredentialsRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import traveller.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserCredentialsRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT true FROM UserCredentials uc WHERE uc.user.login = :login AND uc.password = :password")
+    @Query("SELECT CASE WHEN uc is NULL THEN false ELSE true END " +
+            "FROM UserCredentials uc WHERE uc.user.login = :login AND uc.password = :password")
     Boolean loginCorrect(@Param("login") String login, @Param("password") String password);
     User findByLogin(String login);
 }
