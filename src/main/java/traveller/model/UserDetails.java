a9c754fa1889cf1f89a05e9f12f74e393a988daf
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 09:38:01 +0100

    create UserDetails entity

--- src/main/java/traveller/model/UserDetails.java
+++ src/main/java/traveller/model/UserDetails.java
@@ -1,10 +1,19 @@
 package traveller.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users_details")
 public class UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @OneToOne(optional = false, fetch = FetchType.EAGER)
     private User user;
 
     public Integer getId() {
