3b4bde7b88641e8c9f160c45cdf8604fa96b4e19
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 20:45:38 +0100

    add LoginService

---
+++ src/main/java/traveller/services/LoginService.java
@@ -0,0 +1,30 @@
+package traveller.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import traveller.dtos.UserDTO;
+import traveller.model.User;
+import traveller.repositories.UserCredentialsRepository;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    UserCredentialsRepository userCredentialsRepository;
+
+    public Boolean checkCredentials(String login, String password) {
+
+        Boolean check = userCredentialsRepository.loginCorrect(login, password);
+        return check != null ? check : false;
+    }
+    public UserDTO login(String login) {
+
+        User user = userCredentialsRepository.findByLogin(login);
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setLogin(user.getLogin());
+        userDTO.setEmail(user.getEmail());
+        return userDTO;
+    }
+
+}
