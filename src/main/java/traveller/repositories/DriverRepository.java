a4937b45f1bf64a2aaaab246c35982942bd12ce3
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 23:11:38 +0100

    add DriverRepository

---
+++ src/main/java/traveller/repositories/DriverRepository.java
@@ -0,0 +1,7 @@
+package traveller.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import traveller.model.Driver;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
