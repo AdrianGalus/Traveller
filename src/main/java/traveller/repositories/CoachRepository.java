d568d2ac5d467ec9ea9af08126aa247112766f07
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 22:39:51 +0100

    correct query

--- src/main/java/traveller/repositories/CoachRepository.java
+++ src/main/java/traveller/repositories/CoachRepository.java
@@ -2,10 +2,11 @@ package traveller.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import traveller.model.Coach;
 
 public interface CoachRepository extends JpaRepository<Coach, Long> {
 
-    @Query("SELECT true FROM Coach c WHERE c.registrationNumber = :registerNumber")
-    Boolean isRegisterNumberUsed(String registerNumber);
+    @Query("SELECT true FROM Coach c WHERE c.registrationNumber = :registrationNumber")
+    Boolean isRegistrationNumberUsed(@Param("registrationNumber") String registrationNumber);
 }
