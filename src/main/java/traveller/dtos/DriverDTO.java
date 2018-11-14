071d3163d7bc60c201c52356d82c42667ee109d2
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 10:36:24 +0100

    add DriverDTO

---
+++ src/main/java/traveller/dtos/DriverDTO.java
@@ -0,0 +1,70 @@
+package traveller.dtos;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import traveller.model.Tour;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public class DriverDTO {
+
+    private Integer id;
+    @NotNull @NotBlank @Size(min = 3)
+    private String firstName;
+    @NotNull @NotBlank @Size(min = 3)
+    private String lastName;
+    @NotNull @NotBlank
+    private String phone;
+    @NotNull @NotBlank @Email
+    private String email;
+    private List<Tour> tours;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+    public String getFirstName() {
+
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+    public String getPhone() {
+
+        return phone;
+    }
+    public void setPhone(String phone) {
+
+        this.phone = phone;
+    }
+    public String getEmail() {
+
+        return email;
+    }
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+    public List<Tour> getTours() {
+
+        return tours;
+    }
+    public void setTours(List<Tour> tours) {
+
+        this.tours = tours;
+    }
+}
