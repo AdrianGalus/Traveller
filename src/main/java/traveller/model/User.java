6d8b1328e577d441e5292f776a3fe3d1ada3a711
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 09:37:25 +0100

    create User entity

--- src/main/java/traveller/model/User.java
+++ src/main/java/traveller/model/User.java
@@ -1,9 +1,17 @@
 package traveller.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, unique = true)
     private String login;
+    @Column(nullable = false, unique = true)
     private String email;
 
     public Integer getId() {
