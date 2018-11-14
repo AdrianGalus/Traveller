3ebeb3d53fc4801cc6310d301ed1b3ba150d505b
AdrianGalus <adriangalus8@gmail.com>
2018-11-14 23:11:19 +0100

    add DriverController

---
+++ src/main/java/traveller/controllers/DriverController.java
@@ -0,0 +1,43 @@
+package traveller.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import traveller.dtos.DriverDTO;
+import traveller.dtos.UserDTO;
+import traveller.services.DriverService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/driver")
+public class DriverController {
+
+    @Autowired
+    DriverService driverService;
+
+    @GetMapping("/add")
+    public String addCoach(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser, Model model) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        model.addAttribute("driverForm", new DriverDTO());
+        return "driver";
+    }
+    @PostMapping("/add")
+    public String addCoach(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUSer,
+                                @ModelAttribute("driverForm") @Valid DriverDTO form, BindingResult result) {
+
+        if(loggedUSer == null) {
+            return "redirect:/home";
+        }
+        if(result.hasErrors()) {
+            return "driver";
+        }
+        driverService.addDriver(form);
+        return "redirect:/home";
+    }
+}
