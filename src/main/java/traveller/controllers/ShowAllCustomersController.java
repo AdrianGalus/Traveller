c7ebec742133ff8802f7170dd2295c4d70a32d3f
AdrianGalus <adriangalus8@gmail.com>
2018-11-15 21:07:11 +0100

    add ShowAllCustomersController

---
+++ src/main/java/traveller/controllers/ShowAllCustomersController.java
@@ -0,0 +1,29 @@
+package traveller.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import traveller.dtos.UserDTO;
+import traveller.services.ShowAllCustomersService;
+
+@Controller
+@RequestMapping("/show-all-customers")
+public class ShowAllCustomersController {
+
+    @Autowired
+    ShowAllCustomersService showAllCustomersService;
+
+    @GetMapping
+    public String showAllCustomers(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser,
+                                 Model model) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        model.addAttribute("allCustomers", showAllCustomersService.showAllCustomers());
+        return "all-customers";
+    }
+}
