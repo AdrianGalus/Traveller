f3295a942615c8c7ae1739842a6a9ab9ce1135a2
AdrianGalus <adriangalus8@gmail.com>
2018-11-16 12:10:43 +0100

    rename service

--- src/main/java/traveller/services/CustomerService.java
+++ src/main/java/traveller/services/CustomerService.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class ShowAllCustomersService {
+public class CustomerService {
 
     @Autowired
     CustomerDetailsRepository customerDetailsRepository;
