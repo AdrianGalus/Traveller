c26f7405bc030238f357779dfd75e778d6ba24c0
AdrianGalus <adriangalus8@gmail.com>
2018-11-20 00:32:28 +0100

    add query

--- src/main/java/traveller/repositories/TourDetailsRepository.java
+++ src/main/java/traveller/repositories/TourDetailsRepository.java
@@ -1,7 +1,12 @@
 package traveller.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import traveller.model.TourDetails;
 
 public interface TourDetailsRepository extends JpaRepository<TourDetails, Long> {
+
+    @Query("SELECT td FROM TourDetails td WHERE td.tour.id = :id")
+    TourDetails findByTourId(@Param("id") Long id);
 }
