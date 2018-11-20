9e53a1abb1e099d1383cfa70fa70e25dabf6eafc
AdrianGalus <adriangalus8@gmail.com>
2018-11-20 15:45:25 +0100

    add annotation - Transactional

--- src/main/java/traveller/services/LoginService.java
+++ src/main/java/traveller/services/LoginService.java
@@ -2,11 +2,13 @@ package traveller.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import traveller.dtos.UserDTO;
 import traveller.model.User;
 import traveller.repositories.UserCredentialsRepository;
 
 @Service
+@Transactional
 public class LoginService {
 
     @Autowired
