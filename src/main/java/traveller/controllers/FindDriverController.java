3e2377318048679c0ff538784954cf8bcd47cdd5
AdrianGalus <adriangalus8@gmail.com>
2018-11-22 00:11:55 +0100

    complete method

--- src/main/java/traveller/controllers/FindDriverController.java
+++ src/main/java/traveller/controllers/FindDriverController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import traveller.dtos.UserDTO;
@@ -24,6 +25,23 @@ public class FindDriverController {
             return "redirect:/home";
         }
         model.addAttribute("allDrivers", driverService.findAllDrivers());
+        return "all-drivers";
+    }
+    @GetMapping("/{dto}/{id}")
+    public String findAllDriversByDtoId(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser,
+                                         Model model, @PathVariable String dto, @PathVariable Long id) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        switch(dto) {
+            case "tourDTO":
+                model.addAttribute("allDrivers", driverService.findAllDriversByTourId(id));
+                break;
+            case "coachDTO":
+                model.addAttribute("allDrivers", driverService.findAllDriversByCoachId(id));
+                break;
+        }
         return "all-drivers";
     }
 }
