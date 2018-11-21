93730c188806af57108e7f99f3f318a592d611a4
AdrianGalus <adriangalus8@gmail.com>
2018-11-21 17:26:11 +0100

    add query

--- src/main/java/traveller/repositories/CustomerRepository.java
+++ src/main/java/traveller/repositories/CustomerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import traveller.model.Customer;
 
-public interface CustomerRepoistory extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Query("SELECT true FROM Customer c WHERE c.nip = :nip")
     Boolean isNipUsed(@Param("nip") String nip);
