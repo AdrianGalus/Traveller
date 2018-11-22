22d5e62b08a290b5391fbddbd30af548275cd8b7
AdrianGalus <adriangalus8@gmail.com>
2018-11-22 20:49:15 +0100

    modify queries

--- src/main/java/traveller/repositories/DriverRepository.java
+++ src/main/java/traveller/repositories/DriverRepository.java
@@ -2,10 +2,8 @@ package traveller.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import traveller.model.Driver;
-import java.util.List;
 
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 
     Driver findById(Long id);
-    List<Driver> findAllByCoachId(Long id);
 }
