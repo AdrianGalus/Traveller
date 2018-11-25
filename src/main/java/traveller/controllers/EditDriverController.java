230ed5424538d9fdd4a97bf6aac1f7c7001f7419
AdrianGalus <adriangalus8@gmail.com>
2018-11-25 21:54:57 +0100

    add EditDriverController

---
+++ src/main/java/traveller/controllers/EditDriverController.java
@@ -0,0 +1,73 @@
+package traveller.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import traveller.dtos.DriverDTO;
+import traveller.dtos.UserDTO;
+import traveller.services.CoachService;
+import traveller.services.DriverService;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/edit-driver")
+public class EditDriverController {
+
+    @Autowired
+    DriverService driverService;
+    @Autowired
+    CoachService coachService;
+
+    @GetMapping("/{id}")
+    public String editDriver(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser, Model model,
+                             @PathVariable("id") Long id) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        model.addAttribute("availableCoaches", coachService.findAvailableCoaches());
+        model.addAttribute("driverForm", driverService.findDetails(id));
+        return "edit-driver";
+    }
+    @PostMapping("/{id}")
+    public String confirmEditDriver(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser, Model model,
+                    @ModelAttribute("driverForm") @Valid DriverDTO form, BindingResult result, @PathVariable("id") Long id) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        if(result.hasErrors()) {
+            return "edit-driver";
+        }
+        if(checkPhone(form)) {
+            result.rejectValue("phone", "errors.invalid", "Ten numer telefonu już jest w bazie");
+            return "edit-driver";
+        }
+        if(checkEmail(form)) {
+            result.rejectValue("email", "errors.invalid", "Ten email już jest w bazie");
+            return "edit-driver";
+        }
+        driverService.editDriver(form);
+        return "redirect:/home";
+    }
+    private boolean checkPhone(DriverDTO form) {
+
+        boolean existedPhone = driverService.checkPhone(form.getPhone());
+        String phoneByDriverId = driverService.findPhoneByDriverId(form.getId());
+        if(existedPhone && !phoneByDriverId.equals(form.getPhone())) {
+            return true;
+        }
+        return false;
+    }
+    private boolean checkEmail(DriverDTO form) {
+
+        boolean existedEmail = driverService.checkEmail(form.getEmail());
+        String emailByDriverId = driverService.findEmailByDriverId(form.getId());
+        if(existedEmail && !emailByDriverId.equals(form.getEmail())) {
+            return true;
+        }
+        return false;
+    }
+}
