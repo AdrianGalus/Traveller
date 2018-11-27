22f86268ed4ebcf249b7f693c59d124f21102b90
AdrianGalus <adriangalus8@gmail.com>
2018-11-27 15:21:16 +0100

    correct deleteCustomer method

--- src/main/java/traveller/controllers/DeleteCustomerController.java
+++ src/main/java/traveller/controllers/DeleteCustomerController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import traveller.dtos.CustomerDTO;
+import traveller.dtos.TourDTO;
 import traveller.dtos.UserDTO;
 import traveller.services.CustomerService;
+import traveller.services.TourService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/delete-customer")
@@ -16,6 +21,8 @@ public class DeleteCustomerController {
 
     @Autowired
     CustomerService customerService;
+    @Autowired
+    TourService tourService;
 
     @GetMapping("/{id}")
     public String deleteCustomer(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser, Model model,
@@ -23,6 +30,11 @@ public class DeleteCustomerController {
 
         if(loggedUser == null) {
             return "redirect:/home";
+        }
+        List<TourDTO> tours = tourService.findAllToursByCustomerId(id);
+        if(tours.size() > 0) {
+            model.addAttribute("error", "Ten klient ma zamówioną wycieczkę");
+            return "warnings";
         }
         model.addAttribute("id", id);
         model.addAttribute("type", "customer");
