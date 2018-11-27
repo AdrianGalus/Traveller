5a5c7e2623fa6440e4c53cd9c08ec08449d49a3c
AdrianGalus <adriangalus8@gmail.com>
2018-11-27 16:35:33 +0100

    add DeleteTourController

---
+++ src/main/java/traveller/controllers/DeleteTourController.java
@@ -0,0 +1,41 @@
+package traveller.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import traveller.dtos.UserDTO;
+import traveller.services.TourService;
+
+@Controller
+@RequestMapping("/delete-tour")
+public class DeleteTourController {
+
+    @Autowired
+    TourService tourService;
+
+    @GetMapping("/{id}")
+    public String deleteTour(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser, Model model,
+                             @PathVariable("id") Long id) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        model.addAttribute("id", id);
+        model.addAttribute("type", "tour");
+        return "confirm-delete";
+    }
+    @GetMapping("/confirm/{id}")
+    public String confirmDeleteTour(@SessionAttribute(value = "loggedUser", required = false) UserDTO loggedUser,
+                                    @PathVariable("id") Long id) {
+
+        if(loggedUser == null) {
+            return "redirect:/home";
+        }
+        tourService.deleteTour(id);
+        return "redirect:/home";
+    }
+}
